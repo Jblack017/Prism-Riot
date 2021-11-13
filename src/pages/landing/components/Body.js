1809f5e5e1074c59f707e2dd2b14ffaef2f52c2a
Jblack017 <blackhamjoshua@comcast.net>
2021-11-13 15:23:54 -0700

    Updates

--- src/pages/landing/components/Body.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Body() {
-  return <div className='row-start-2 row-end-6'>Body</div>;
-}
--- src/pages/landing/components/Nav.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Nav() {
-  return (
-    <div className='bg-Dark-Purple h-full w-full text-4xl'>
-      Prism Riot Hair Studio
-    </div>
-  );
-}
