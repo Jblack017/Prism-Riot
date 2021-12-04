e62fe9be747c70be4fa77c2e3d2b3643de7a87bc
Jblack017 <blackhamjoshua@comcast.net>
2021-12-04 10:13:36 -0700

    adds pages links logos

--- src/pages/landing/components/Hexagon.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Hexagon() {
-  return (
-    <div className='absolute place-self-end h-24 w-24 z-1'>
-      <div className='hexagon'></div>
-    </div>
-  );
-}
--- src/pages/landing/components/Main.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Main() {
-  return <div className='bg-Dark-Purple row-start-2 row-end-7'>Body</div>;
-}
